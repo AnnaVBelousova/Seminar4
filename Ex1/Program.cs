512961dfcda50c3bdb32f5ef15d8ab8454eedcde
anna <a1b2v3@mail.ru>
2022-12-12 23:42:32 +0500

    add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+double A = Convert.ToDouble(Console.ReadLine());
+double B = Convert.ToDouble(Console.ReadLine());
+double count = 1;
+for (int i = 1; i <= B; i++)
+{
+  count = count * A;
+}
+
+Console.WriteLine(count);
