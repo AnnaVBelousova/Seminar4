51d9486f4821b8eec83ac37239a551a4ca8aab73
anna <a1b2v3@mail.ru>
2022-12-13 00:06:24 +0500

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int n = Convert.ToInt32(Console.ReadLine());
+
+int counter = 0 ;
+while (n!=0)
+{
+  int a = n % 10;
+  counter = counter + a;
+  n = n / 10;
+}
+ 
+ Console.WriteLine(counter);
